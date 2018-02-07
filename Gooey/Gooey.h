7337b435d5a2247d4adc76d7686d401d225328de
James Power <jim.power@duethealth.com>
2018-02-07 16:06:45 -0500

    Initial Commit

---
+++ Gooey/Gooey.h
@@ -0,0 +1,19 @@
+//
+//  Gooey.h
+//  Gooey
+//
+//  Created by James Power on 2/7/18.
+//  Copyright © 2018 Duet Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gooey.
+FOUNDATION_EXPORT double GooeyVersionNumber;
+
+//! Project version string for Gooey.
+FOUNDATION_EXPORT const unsigned char GooeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gooey/PublicHeader.h>
+
+
