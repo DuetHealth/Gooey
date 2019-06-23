50cefbfbc875f2a00c418dc14dcaec2c1f7f1d63
Kesi Maduka <me@kez.io>
2019-06-22 20:34:34 -0400

    Workaround for hodgepodge arrays to be activated / deactivated.

--- Gooey/Sources/ConstraintRepresentable.swift
+++ Gooey/Sources/ConstraintRepresentable.swift
@@ -21,6 +21,16 @@ public extension GooeyNamespace where Base == NSLayoutConstraint.Type {
 
     /// The effect of this method is the same as setting the
     ///     [isActive](apple-reference-documentation://hsRIb14KxL) property of
+    ///     each constraint to `true`.
+    ///
+    /// - Parameter constraintRepresentables: Constraint representable
+    ///      conforming objects to be activated.
+    func activate(_ constraintRepresentables: [ConstraintRepresentable]) {
+        NSLayoutConstraint.activate(constraintRepresentables.reduce(into: [], { $0.append(contentsOf: $1.constraints) }))
+    }
+
+    /// The effect of this method is the same as setting the
+    ///     [isActive](apple-reference-documentation://hsRIb14KxL) property of
     ///     each constraint to `false`.
     ///
     /// - Parameter constraintRepresentable: A constraint representable
@@ -29,20 +39,22 @@ public extension GooeyNamespace where Base == NSLayoutConstraint.Type {
         NSLayoutConstraint.deactivate(constraintRepresentable.constraints)
     }
 
+    /// The effect of this method is the same as setting the
+    ///     [isActive](apple-reference-documentation://hsRIb14KxL) property of
+    ///     each constraint to `false`.
+    ///
+    /// - Parameter constraintRepresentables: Constraint representable
+    ///      conforming objects to be activated.
+    func deactivate(_ constraintRepresentables: [ConstraintRepresentable]) {
+        NSLayoutConstraint.deactivate(constraintRepresentables.reduce(into: [], { $0.append(contentsOf: $1.constraints) }))
+    }
+
 }
 
 extension NSLayoutConstraint: ConstraintRepresentable {
 
     public var constraints: [NSLayoutConstraint] {
         return [self]
-    }
-
-}
-
-extension Array: ConstraintRepresentable where Element: ConstraintRepresentable {
-
-    public var constraints: [NSLayoutConstraint] {
-        return reduce(into: [], { $0.append(contentsOf: $1.constraints) })
     }
 
 }
