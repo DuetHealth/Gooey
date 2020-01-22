049ef52f463577e6949000b680323a88d69d6b38
kevin.li <kevin.li@duethealth.com>
2020-01-22 14:50:50 -0500

    Source location

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     targets: [
         .target(
             name: "Gooey",
-            dependencies: []),
+            dependencies: [],
+            path: "Gooey"),
     ]
 )
